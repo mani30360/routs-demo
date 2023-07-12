bc135fb63828a8e94559819806cc82ac334323b4
mani30360 <manikantakare9@gmail.comm>
2023-07-12 17:12:55 +0530

    update code

--- src/User.jsx
+++ src/User.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import data from './data.json';
-const user = () => {
+
+const User = ({ items }) => {
+    const [sortedData, setSortedData] = useState(data);
+    const [sortOrder, setSortOrder] = useState('ascending');
+
+    const handleSort = () => {
+        const sorted = [...sortedData].sort((a, b) =>
+            a.name.localeCompare(b.name)
+        );
+
+        if (sortOrder === 'descending') {
+            sorted.reverse();
+            setSortOrder('ascending');
+        } else {
+            setSortOrder('descending');
+        }
+
+        setSortedData(sorted);
+    };
+
     return (
         <div className='container'>
-            {data.map((item) => (
-                <div key={item.id} >
+
+            <button onClick={handleSort} className="btn btn-primary" >Sort {sortOrder === 'ascending' ? 'Descending' : 'Ascending'}</button>
+
+            {sortedData.map((item) => (
+                <div key={item.id}>
                     <h2>{item.name}</h2>
                     <p>{item.Email}</p>
                     <p>{item.Phone}</p>
@@ -14,4 +36,4 @@ const user = () => {
     );
 };
 
-export default user;
+export default User;
